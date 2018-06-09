b0a81c6c6bb4e933dc0b357a4bc04aff984fc459
Alexey Kott <alexey.kott@gmail.com>
2018-06-09 20:29:41 +0300

    ...

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
 import csv
+import sys
 from pathlib import Path
 from tkinter import filedialog, Tk
+import json
+import win32con, win32api
+
+
+
+def load_settings():
+    if sys.platform == 'linux':
+        with open("./.settings") as file:
+            settings_text = json.loads(file.read())
+    else:
+        with
 
 
 def transform_csv_file(file_name, dest_dir):
@@ -15,8 +27,21 @@ def transform_csv_file(file_name, dest_dir):
 
 if __name__ == "__main__":
     root = Tk()
+    settings = load_settings()
+
     source_dir = Path(filedialog.askdirectory(title="Откуда взять файлы?"))
     dest_dir = Path(filedialog.askdirectory(title="Куда сложить файлы?"))
+
+    with open(".settings", "w") as outfile:
+        settings = {
+            'source_dir': source_dir,
+            'dest_dir': dest_dir
+        }
+        json.dump(settings, outfile)
+
+    if sys.platform != 'linux':
+        win32api.SetFileAttributes(".settings", win32con.FILE_ATTRIBUTE_HIDDEN)
+
 
     csv_files = [entry for entry in source_dir.iterdir() if entry.suffix == ".csv"]
     for csv_file in csv_files:
