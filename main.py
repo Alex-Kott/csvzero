7242d1f6d8ec7d25a23417ae895d00e8db8f5062
Alexey Kott <alexey.kott@gmail.com>
2018-06-09 19:21:28 +0300

    ...

--- main.py
+++ main.py
@@ -1,26 +1,23 @@
 import csv
-import os
 from pathlib import Path
+from tkinter import filedialog, Tk
 
 
-def transform_csv_file(file_name):
+def transform_csv_file(file_name, dest_dir):
     with open(file_name) as read_file:
         csvreader = csv.reader(read_file, delimiter=',')
-        with open(f"{file_name.stem}_tmp.csv", "w") as write_file:
+        with open(dest_dir / file_name.name, "w") as write_file:
             csvwriter = csv.writer(write_file, delimiter=",")
             for row in csvreader:
+                print(row)
                 csvwriter.writerow([row[0], "000000", *row[1:]])
-
-    os.remove(file_name)
-    os.rename(f"{file_name.stem}_tmp.csv", file_name)
 
 
 if __name__ == "__main__":
-    from tkinter import filedialog, Tk
-
     root = Tk()
-    dir_name = Path(filedialog.askdirectory())
+    source_dir = Path(filedialog.askdirectory(title="Откуда взять файлы?"))
+    dest_dir = Path(filedialog.askdirectory(title="Куда сложить файлы?"))
 
-    csv_files = [entry for entry in dir_name.iterdir() if entry.suffix == ".csv"]
+    csv_files = [entry for entry in source_dir.iterdir() if entry.suffix == ".csv"]
     for csv_file in csv_files:
-        transform_csv_file(csv_file)
+        transform_csv_file(csv_file, dest_dir)
